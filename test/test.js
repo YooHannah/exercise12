5938e1dec31b0d8b571a0e6b36fa4660560e0253
YooHannah <1942063705@qq.com>
2017-09-26 15:46:35 +0800

    finish

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
 const assert = require('assert')
-
+console.log(222)
 let mod = null
 global.define = function (fn) {
   const module = { exports: {} }
   fn(module, module.exports)
   mod = module
 }
-
+console.log(111,mod)
 describe('loader', function () {
   it('可以通过 define 拿到数据', function () {
     require('../dist/index')
+    console.log(mod)
     assert.equal(mod.exports, 'hello world')
   })
 })
